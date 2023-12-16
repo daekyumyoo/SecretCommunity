387079c6255affecc14398257a05d43846f38d47
daekyumyoo <daekyum.dev@gmail.com>
2023-12-16 19:43:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/secretcommunity/SecretCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.secretcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretCommunityApplication.class, args);
+    }
+
+}
